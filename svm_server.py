322c1eff5431d172284ebdd0f431b427900bb8c8
micuat <micuat@aim.com>
2016-04-08 19:23:00 -0400

    Replace muse powerbands with algorithm from  bci_workshop

--- svm_server.py
+++ svm_server.py
@@ -29,7 +29,7 @@ server.handle_timeout = types.MethodType(handle_timeout, server)
 
 class Dataset:
     state = "none"
-    maxSampleNum = 50
+    maxSampleNum = 30
     classifier_ready = False
 
     def __init__(self):
@@ -48,34 +48,94 @@ class Dataset:
 
     def record(self, sample):
         if self.state == "recording_initial":
-            self.feat_matrix = np.copy(sample)
+            self.feat_matrix = [np.copy(sample)]
             self.state = "recording"
         elif self.state == "recording":
-            self.feat_matrix = np.concatenate((self.feat_matrix, sample))
+            self.feat_matrix = np.concatenate((self.feat_matrix, [sample]), axis=0)
             if self.feat_matrix.shape[0] >= self.maxSampleNum:
                 print "done"
+                print self.feat_matrix
                 self.state = "done"
 
-feat_vector = np.array([[0.0, 0.0, 0.0, 0.0]])
-feat_vector_update = [0, 0, 0, 0]
+def control_record_callback(path, tags, args, source):
+    command = path.split("/")[4]
+    if command == "1":
+        print "1st sample set"
+        m = OSCMessage("/bci_art/svm/start/1/received")
+        client.send(m)
+        datasets[0].startRecording()
+    elif command == "2":
+        print "2nd sample set"
+        m = OSCMessage("/bci_art/svm/start/2/received")
+        client.send(m)
+        datasets[1].startRecording()
+
+def quit_callback(path, tags, args, source):
+    # don't do this at home (or it'll quit blender)
+    global run
+    run = False
+
+eegArray = []
 datasets = [Dataset(), Dataset()]
 
-def power_abs_callback(path, tags, args, source):
-    powerBand = path.split("/")[3]
-    if powerBand == "delta_absolute" and feat_vector[0][0] != args[1]:
-        feat_vector[0][0] = args[1]
-        feat_vector_update[0] = 1
-    elif powerBand == "theta_absolute" and feat_vector[0][1] != args[1]:
-        feat_vector[0][1] = args[1]
-        feat_vector_update[1] = 1
-    elif powerBand == "alpha_absolute" and feat_vector[0][2] != args[1]:
-        feat_vector[0][2] = args[1]
-        feat_vector_update[2] = 1
-    elif powerBand == "beta_absolute" and feat_vector[0][3] != args[1]:
-        feat_vector[0][3] = args[1]
-        feat_vector_update[3] = 1
+def compute_feature_vector(eegdata, Fs):
+    # https://github.com/bcimontreal/bci_workshop/blob/master/bci_workshop_tools.py
+    # print eegdata
+    eegdata = np.array([eegdata]).T
+    
+    # 1. Compute the PSD
+    winSampleLength = len(eegdata)
+    
+    # Apply Hamming window
+    w = np.hamming(winSampleLength)
+    dataWinCentered = eegdata - np.mean(eegdata, axis=0) # Remove offset
+    dataWinCenteredHam = (dataWinCentered.T*w).T
 
-    if np.all(feat_vector_update) == 1:
+    NFFT = nextpow2(winSampleLength)
+    Y = np.fft.fft(dataWinCenteredHam, n=NFFT, axis=0)/winSampleLength
+    PSD = 2*np.abs(Y[0:NFFT/2,:])
+    f = Fs/2*np.linspace(0,1,NFFT/2)
+    
+    # SPECTRAL FEATURES
+    # Average of band powers
+    # Delta <4
+    ind_delta, = np.where(f<4)
+    meanDelta = np.mean(PSD[ind_delta,:],axis=0)
+    # Theta 4-8
+    ind_theta, = np.where((f>=4) & (f<=8))
+    meanTheta = np.mean(PSD[ind_theta,:],axis=0)
+    # Alpha 8-12
+    ind_alpha, = np.where((f>=8) & (f<=12)) 
+    meanAlpha = np.mean(PSD[ind_alpha,:],axis=0)
+    # Beta 12-30
+    ind_beta, = np.where((f>=12) & (f<30))
+    meanBeta = np.mean(PSD[ind_beta,:],axis=0)
+    
+    feature_vector = np.concatenate((meanDelta, meanTheta, meanAlpha, meanBeta),axis=0)
+    
+    feature_vector = np.log10(feature_vector)   
+       
+    return feature_vector
+
+def nextpow2(i):
+    """ 
+    Find the next power of 2 for number i
+    
+    """
+    n = 1
+    while n < i: 
+        n *= 2
+    return n
+
+def eeg_callback(path, tags, args, source):
+    eeg = args[1]
+    # print eeg
+    global eegArray
+    if eegArray == []:
+        eegArray = [eeg]
+    elif len(eegArray) == 220:
+        feat_vector = compute_feature_vector(eegArray, 220)
+        
         datasets[0].record(feat_vector)
         datasets[1].record(feat_vector)
         
@@ -97,37 +157,15 @@ def power_abs_callback(path, tags, args, source):
             client.send(m)
         
         print feat_vector
-        feat_vector_update[0] = 0
-        feat_vector_update[1] = 0
-        feat_vector_update[2] = 0
-        feat_vector_update[3] = 0
-
-def control_record_callback(path, tags, args, source):
-    command = path.split("/")[4]
-    if command == "1":
-        print "1st sample set"
-        m = OSCMessage("/bci_art/svm/start/1/received")
-        client.send(m)
-        datasets[0].startRecording()
-    elif command == "2":
-        print "2nd sample set"
-        m = OSCMessage("/bci_art/svm/start/2/received")
-        client.send(m)
-        datasets[1].startRecording()
-
-def quit_callback(path, tags, args, source):
-    # don't do this at home (or it'll quit blender)
-    global run
-    run = False
+        eegArray = [eeg]
+    else:
+        eegArray = np.concatenate((eegArray, [eeg]))
 
 def default_callback(path, tags, args, source):
     # do nothing
     return
 
-server.addMsgHandler( "/muse/elements/delta_absolute", power_abs_callback )
-server.addMsgHandler( "/muse/elements/theta_absolute", power_abs_callback )
-server.addMsgHandler( "/muse/elements/alpha_absolute", power_abs_callback )
-server.addMsgHandler( "/muse/elements/beta_absolute", power_abs_callback )
+server.addMsgHandler( "/muse/eeg", eeg_callback )
 server.addMsgHandler( "/bci_art/svm/start/1", control_record_callback )
 server.addMsgHandler( "/bci_art/svm/start/2", control_record_callback )
 server.addMsgHandler( "/bci_art/quit", quit_callback )
