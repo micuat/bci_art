220ccec4dc6a6f17a933f6013ff7af2790948364
micuat <micuat@aim.com>
2016-04-14 21:26:36 -0400

    Plot and realtime data interpolation

--- tsne_server.py
+++ tsne_server.py
@@ -53,6 +53,7 @@ feat_matrix = []
 def plot_tsne():
     print feat_matrix
     print feat_matrix.shape
+    global tsneResult
     tsneResult = tsne.tsne(feat_matrix, 2, 50, 20.0);
     print tsneResult
 
@@ -67,18 +68,61 @@ def plot_tsne():
     plt.plot(tsneResult[n1:,0], tsneResult[n1:,1], 'bo')
     plt.show()
 
+tsne_ready = False
+
 def on_feature_vector(feat_vector):
     print feat_vector
     
+    global tsne_ready
     global feat_matrix
-    if feat_matrix == []:
-        feat_matrix = np.matrix(feat_vector)
+    if tsne_ready == False:
+        if feat_matrix == []:
+            feat_matrix = np.matrix(feat_vector)
+        else:
+            feat_matrix = np.concatenate((feat_matrix, [feat_vector]))
+        
+        if feat_matrix.shape[0] == 10:
+            plot_tsne()
+            tsne_ready = True
     else:
-        feat_matrix = np.concatenate((feat_matrix, [feat_vector]))
-    
-    if feat_matrix.shape[0] == 60:
-        plot_tsne()
-        feat_matrix = []
+        closestDistance0 = 10000000000.0
+        closestDistance1 = 10000000000.0
+        closestDistance2 = 10000000000.0
+        closestIndex0 = 0
+        closestIndex1 = 0
+        closestIndex2 = 0
+        for i in range(0, np.size(feat_matrix, 0)):
+            distance = np.linalg.norm(feat_matrix[i, :] - feat_vector)
+            if distance < closestDistance0:
+                closestDistance2 = closestDistance1
+                closestDistance1 = closestDistance0
+                closestDistance0 = distance
+                closestIndex2 = closestIndex1
+                closestIndex1 = closestIndex0
+                closestIndex0 = i
+            elif distance < closestDistance1:
+                closestDistance2 = closestDistance1
+                closestDistance1 = distance
+                closestIndex2 = closestIndex1
+                closestIndex1 = i
+            elif distance < closestDistance2:
+                closestDistance2 = distance
+                closestIndex2 = i
+        print `closestIndex0` + " " + `closestIndex1` + " " + `closestIndex2`
+        print tsneResult[closestIndex0, :]
+        print tsneResult[closestIndex1, :]
+        print tsneResult[closestIndex2, :]
+
+        closestDistanceTotal = closestDistance0 + closestDistance1 + closestDistance2
+        interpolated = (tsneResult[closestIndex0, :] * (closestDistance1 + closestDistance2) + tsneResult[closestIndex1, :] * (closestDistance1 + closestDistance0) + tsneResult[closestIndex2, :] * (closestDistance0 + closestDistance1)) / closestDistanceTotal * 0.5
+        print interpolated
+
+        m = OSCMessage("/muse/tsne")
+        m.append(interpolated[0])
+        m.append(interpolated[1])
+        m.append(closestIndex0)
+        #client.send(m)
+
 
 
 mp.set_on_feature_vector(on_feature_vector)
