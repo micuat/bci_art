0490b76a46641b3b9685fc61b29b5c5133c26859
micuat <micuat@gmail.com>
2017-06-13 21:37:06 -0400

    tsne python3 version; svm not yet

--- tsne_server.py
+++ tsne_server.py
@@ -1,69 +1,41 @@
 # Muse command
 # muse-io --device Muse-7042 --osc 'osc.udp://localhost:12000'
 
-from OSC import OSCServer, OSCClient, OSCMessage, OSCClientError
 import sys
 from time import sleep
+import time
 import numpy as np
 from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
-import time
+from pythonosc import udp_client
 import musepy
 
 port_muse = int(sys.argv[1])
 port_node = int(sys.argv[2])
 
-# muse-io server
-server = OSCServer( ("localhost", port_muse) )
-server.timeout = 0
-
 # musepy
-mp = musepy.Musepy(server)
+mp = musepy.Musepy(port_muse)
 
 # openFrameworks
-client = OSCClient()
-client.connect( ("localhost", port_node) )
+client = udp_client.SimpleUDPClient("127.0.0.1", port_node)
 
 run = True
-
-# this method of reporting timeouts only works by convention
-# that before calling handle_request() field .timed_out is
-# set to False
-def handle_timeout(self):
-    self.timed_out = True
-
-# funny python's way to add a method to an instance of a class
-import types
-server.handle_timeout = types.MethodType(handle_timeout, server)
-
-def quit_callback(path, tags, args, source):
-    # don't do this at home (or it'll quit blender)
-    global run
-    run = False
-
-def default_callback(path, tags, args, source):
-    # do nothing
-    return
 
 def normalize(p):
     max = np.amax(p)
     min = np.amin(p)
     return (p - min) / (max - min)
 
-server.addMsgHandler( "/bci_art/quit", quit_callback )
-server.addMsgHandler( "default", default_callback )
-
 feat_matrix = []
 
 def plot_tsne():
-    print feat_matrix
-    print feat_matrix.shape
+    print(feat_matrix)
+    print(feat_matrix.shape)
     global tsneResult
     model = TSNE(n_components=2, random_state=0, learning_rate=100)
     tsneResult = model.fit_transform(feat_matrix)
     tsneResult[:,0] = normalize(tsneResult[:,0])
     tsneResult[:,1] = normalize(tsneResult[:,1])
-    print tsneResult
+    print(tsneResult)
 
     timestamp = timestr = time.strftime("%Y%m%d-%H%M%S")
     np.save('t0.npy', feat_matrix)
@@ -71,18 +43,10 @@ def plot_tsne():
     np.save('tsneResult.npy', tsneResult)
     np.save('%stsneResult.npy' % timestamp, tsneResult)
 
-    n0 = np.size(feat_matrix, 0);
-    n0 = tsneResult.shape[0] / 3
-    n1 = n0 * 2
-    plt.plot(tsneResult[0:n0 - 1,0], tsneResult[0:n0 - 1,1], 'yo')
-    plt.plot(tsneResult[n0:n1 - 1,0], tsneResult[n0:n1 - 1,1], 'ro')
-    plt.plot(tsneResult[n1:,0], tsneResult[n1:,1], 'bo')
-    plt.show()
-
 tsne_ready = False
 
 def on_feature_vector(feat_vector):
-    print feat_vector
+    print(feat_vector)
     
     global tsne_ready
     global feat_matrix
@@ -93,7 +57,7 @@ def on_feature_vector(feat_vector):
             feat_matrix = np.concatenate((feat_matrix, [feat_vector]))
         
         if feat_matrix.shape[0] % 10 == 0:
-            print feat_matrix.shape[0]
+            print(feat_matrix.shape[0])
         
         if feat_matrix.shape[0] == 120:
             plot_tsne()
@@ -122,36 +86,23 @@ def on_feature_vector(feat_vector):
             elif distance < closestDistance2:
                 closestDistance2 = distance
                 closestIndex2 = i
-        print `closestIndex0` + " " + `closestIndex1` + " " + `closestIndex2`
-        print tsneResult[closestIndex0, :]
-        print tsneResult[closestIndex1, :]
-        print tsneResult[closestIndex2, :]
+        print(str(closestIndex0) + " " + str(closestIndex1) + " " + str(closestIndex2))
+        print(tsneResult[closestIndex0, :])
+        print(tsneResult[closestIndex1, :])
+        print(tsneResult[closestIndex2, :])
 
         closestDistanceTotal = closestDistance0 + closestDistance1 + closestDistance2
         interpolated = (tsneResult[closestIndex0, :] * (closestDistance1 + closestDistance2) + tsneResult[closestIndex1, :] * (closestDistance1 + closestDistance0) + tsneResult[closestIndex2, :] * (closestDistance0 + closestDistance1)) / closestDistanceTotal * 0.5
-        print interpolated
-
-        m = OSCMessage("/muse/tsne")
-        m.append(interpolated[0])
-        m.append(interpolated[1])
-        m.append(closestIndex0)
-        try:
-            client.send(m)
-        except OSCClientError:
-            print "caught osc error"
-
+        print(interpolated)
+        client.send_message("/muse/tsne", (interpolated[0], interpolated[1], closestIndex0))
 
 mp.set_on_feature_vector(on_feature_vector)
 
 def each_frame():
-    # clear timed_out flag
-    server.timed_out = False
-    # handle all pending requests then return
-    while not server.timed_out:
-        server.handle_request()
+    pass
 
 while run:
     sleep(0.01)
     each_frame()
 
-server.close()
+mp.exit()
